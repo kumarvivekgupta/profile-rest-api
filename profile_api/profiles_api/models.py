aafecd03fc72c879c2901db6daa0a8e8d05d57ee
kumarvivekgupta <v.iv1005ek@gmail.com>
2021-02-21 18:26:02 +0530

    initial commit

---
+++ profile_api/profiles_api/models.py
@@ -0,0 +1,63 @@
+from django.db import models
+
+from django.contrib.auth.models import AbstractBaseUser
+
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
+
+# Create your models here.
+
+class UserProfileManager(BaseUserManager):
+    """Helps Django work with custom yser model"""
+
+    def create_user(self,email,name,passowrd=None):
+        if not email:
+            raise ValueError('User must have a email address')
+    
+        email = self.normalize_email(email)
+        user = self.model(email=email,name=name)
+
+        user.set_password(password)
+
+        user.save(using=self.db)
+        return user
+
+    def create_superuser(self,email,name,password):
+
+        user = self.create_user(email,name,password)
+        user.is_superuser = True
+        user.is_staff = True
+
+        user.save(using=self.db)
+        return user
+    
+
+
+
+class UserProfile(AbstractBaseUser,PermissionsMixin):
+    """Represents a "user progile" of system."""
+
+    email = models.EmailField(max_length=255,unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+ 
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def get_full_name(self):
+        """user to get user full name"""
+
+        return self.name
+
+    def get_short_name(self):
+        """short name of user"""
+        return self.name
+
+    def __str__(self):
+        """django converts this when it needs to convert ovbject to string"""
+
+        return self.email
+    
